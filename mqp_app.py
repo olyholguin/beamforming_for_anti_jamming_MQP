082366f47eb0fc1dadcc5dfac79d2385ca54a126
lauren <rddonati@wpi.edu>
2024-11-14 12:28:30 -0500

    python progress

--- mqp_app.py
+++ mqp_app.py
@@ -24,31 +24,27 @@ def create_signal(time, carrier_freq, colSp, rowSp, noise_pwr, doa):
     ura_x, ura_y = np.meshgrid(np.arange(2) * colSpacing, np.arange(2) * rowSpacing)
     ura = np.column_stack((ura_x.ravel(), ura_y.ravel(), np.zeros(2 * 2)))
     ura = ura[np.nonzero(ura)]
-    # no_zeros = ura[np.nonzero(ura)] 
-    # ura = np.zeros((2, 2))
-    # ura[0,0] = no_zeros[0]
-    # ura[0,1] = no_zeros[1]
-    # ura[1,0] = no_zeros[2]
-    # ura[1,1] = no_zeros[3]
     
     n_elements = ura.shape[0]
-    n_signals = 1
+    n_signals = 300
     n_samples = len(time)
-    output = np.zeros((n_elements, n_samples), dtype=complex)
+    output = np.zeros((n_samples, n_elements), dtype=complex)
     rad = [doa[0]*np.pi/180, doa[1]*np.pi/180]
 
     for i in range(n_signals):
         for j in range(n_elements):
-            delay = np.dot(ura[j], np.array([np.cos(rad[i]), np.sin(rad[i])])) / carrier_freq
-            output[j] += pulse[i] * np.exp(-1j * 2 * np.pi * carrier_freq * delay)
+            delay = np.dot(ura[j], np.array([np.cos(rad[0]), np.sin(rad[1])])) / carrier_freq
+            delay_new = delay[0]
+            output[i,j] += pulse[i] * np.exp(-1j * 2 * np.pi * carrier_freq * delay_new)
     noise = 0
-    return [ura, output, noise]
+    return [ura, output, noise, pulse]
 
-[ura, x, noise] = create_signal(time, carrier_freq, colSp, rowSp, noise_pwr, doa)
+[ura, x, noise, pulse] = create_signal(time, carrier_freq, colSp, rowSp, noise_pwr, doa)
 
-# plt.plot(time, pulse)
+# plt.plot(time, abs(x))
+# plt.plot(time, abs(pulse))
 # plt.xlabel('Time')
 # plt.ylabel('Amplitude')
-# plt.title('Rectangular Pulse')
+# plt.title('Output')
 # plt.grid()
 # plt.show()
