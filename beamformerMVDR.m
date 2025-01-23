93787f01a06a003269979c7489e134a58991283a
Rachel Donati <rachel@autoreg-6747445.dyn.wpi.edu>
2025-01-23 13:47:42 -0500

    small clean up

--- beamformerMVDR.m
+++ beamformerMVDR.m
@@ -1,28 +1,29 @@
 function [yURA, w] = beamformerMVDR(ura, x, noise, doas, t, carrierFreq, propagation_path)
 % beamformerMVDR takes in URA and DoA and outputs signal and weights
+
 mvdrbeamformer = phased.MVDRBeamformer('SensorArray',ura,...
    'Direction',doas,'OperatingFrequency',carrierFreq,...
    'TrainingInputPort',true,'WeightsOutputPort',true);
 rxSignal = x + noise;
 [yURA, w]= mvdrbeamformer(rxSignal, noise);
 
-% figure;
-% plot(t,abs(yURA));
-% axis tight;
-% title(strcat('Output of MVDR Beamformer: ', propagation_path));
-% xlabel('Time (s)');
-% ylabel('Magnitude (V)');
-% xlim([0 0.3])
-% ylim([0 0.3])
+figure;
+plot(t,abs(yURA));
+axis tight;
+title(strcat('Output of MVDR Beamformer: ', propagation_path));
+xlabel('Time (s)');
+ylabel('Magnitude (V)');
+xlim([0 0.3])
+ylim([0 0.3])
 
-% figure(5);
-% p = pattern(ura, carrierFreq, -180:180, 0 ,'Weights', w,'Type','powerdb',...
-%     'PropagationSpeed',physconst('LightSpeed'),'Normalize',false,...
-%     'CoordinateSystem','rectangular');
-% plot(p)
-% title('Response Pattern at 0 Degrees Elevation');
-%
-% figure(6);
-% polarpattern(p);
-% title('MVDR Beam Pattern');
+figure(5);
+p = pattern(ura, carrierFreq, -90:90, 0 ,'Weights', w,'Type','powerdb',...
+    'PropagationSpeed',physconst('LightSpeed'),'Normalize',false,...
+    'CoordinateSystem','rectangular');
+plot(p)
+title('Response Pattern at 0 Degrees Elevation');
+
+figure(6);
+polarpattern(p);
+title('MVDR Beam Pattern');
 end
--- estimateMUSIC.m
+++ estimateMUSIC.m
@@ -1,13 +1,6 @@
 function [doas, averageMatrix] = estimateMUSIC(ura, x, noise, carrierFreq, averageMatrix, i, azimuth_range, elevation_range)
 % estimateMUSIC takes in ura and noise and outputs doa array
 
-% azimuth_range(1) = 180-azimuth_range(1);
-% azimuth_range(2) = 180-azimuth_range(2);
-% a_low = azimuth_range(2);
-% a_high = azimuth_range(1);
-% e_low = elevation_range(1);
-% e_high = elevation_range(2);
-
 a_low = azimuth_range(1);
 a_high = azimuth_range(2);
 e_low = elevation_range(1);
@@ -20,14 +13,6 @@ estimator2D = phased.MUSICEstimator2D('SensorArray',ura,...
     'AzimuthScanAngles',a_low:.5:a_high,...
     'ElevationScanAngles',e_low:.5:e_high);
 
-% Testing different Azimuth Scan Angles
-% estimator2D = phased.MUSICEstimator2D('SensorArray',ura,...
-%     'OperatingFrequency',carrierFreq,...
-%     'NumSignalsSource','Property',...
-%     'DOAOutputPort',true,'NumSignals',1,...
-%     'AzimuthScanAngles',130:-.5:-130,...
-%     'ElevationScanAngles',e_low:.5:e_high);
-
 [~,doas] = estimator2D(x + noise);
 
 % figure;
@@ -37,6 +22,5 @@ estimator2D = phased.MUSICEstimator2D('SensorArray',ura,...
 fprintf("Measured DoA: \t%.2f \t%.2f \n", round(doas(1,1),2), round(doas(2,1),2))
 averageMatrix(i, 1) = doas(1,1);
 averageMatrix(i, 2) = doas(2,1);
-
 
 end
