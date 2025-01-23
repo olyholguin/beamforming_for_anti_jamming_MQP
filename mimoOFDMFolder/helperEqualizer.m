92c51253a0c1796f06dccc6a3255b84188345f1b
Rachel Donati <rachel@autoreg-6747445.dyn.wpi.edu>
2025-01-23 13:34:54 -0500

    files moved

--- mimoOFDMFolder/helperEqualizer.m
+++
@@ -1,9 +0,0 @@
-function rxEq = helperEqualizer(rxOFDM, hD, numTx)
-% This function is only in support of MIMOBeamformingExample.
-% It may be removed in a future release.
-
-% Copyright 2012-2014 The MathWorks, Inc.
-
-num = conj(hD);
-denum=conj(hD).*hD;
-rxEq = (rxOFDM .* num) ./ (numTx * denum);
