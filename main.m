bd36938205aee1718b766c8962829549668f946a
Rachel Donati <rachel@Rachels-MacBook-Air.local>
2025-01-31 12:47:22 -0500

    new beam plot

--- main.m
+++ main.m
@@ -13,9 +13,9 @@ bjammerPwr = 10;
 averageMatrix = zeros(1, 2);
 i = 1;
 show_plots = false;
-sweep_loc = 'b'; % a, b, j, j_up, j_down
+sweep_loc = 'j'; % a, b, j, j_up, j_down
 if strcmp(sweep_loc,'b') || strcmp(sweep_loc,'a') || strcmp(sweep_loc,'j')
-    sweep = zeros(780,3);
+    sweep = zeros(60,3);
     locations = -120:20:120;
 else
    sweep = zeros(360,3);
@@ -43,13 +43,13 @@ propagation_path = " B to A";
 
 jamMatrix = [0.01 0.1 1 10 100 1000];
 iteration = 1;
-locations = 0;
+locations = 50;
 jamMatrix = 10;
 weights = ones(4,1);
 
 for loc = locations
     for jamPwr = jamMatrix
-        for i = 1:1:1
+        for i = 1:1:2
             bjammerPwr = jamPwr;
 
             % Locations of A, B and Jammer
@@ -87,11 +87,11 @@ for loc = locations
             [pathAtoB, pathBtoA, pathJtoA, pathJtoB] = calculateExpected(targetlocA, targetlocB, jammerloc);
 
             % Transmit signal from B to A
-            % disp("weight values " + weights);
-            % w1 = weights(1:2);
-            % w2 = weights(3:4);
-            % uraNewW = phased.URA([2,2],'Taper',[w1,w2], 'ElementSpacing', [1.19916 1.49895]);
-            [rx_xA, noise] = propagateSignal(x, pathBtoA, targetlocB, targetlocA, zeroVelocity, carrierFreq, noisePwr, rs, transmitter, radiator, targetchannel, ura);
+            disp("weight values " + weights);
+            w1 = weights(1:2);
+            w2 = weights(3:4);
+            uraNewW = phased.URA([2,2],'Taper',[w1,w2], 'ElementSpacing', [1.19916 1.49895]);
+            [rx_xA, noise] = propagateSignal(x, pathBtoA, targetlocB, targetlocA, zeroVelocity, carrierFreq, noisePwr, rs, transmitter, radiator, targetchannel, uraNewW);
 
             % Initialize Jammer values
             [jamsig] = startJammer(bjammerPwr, samplingFreq, carrierFreq, ura, jammerloc, targetlocA, zeroVelocity, pathJtoA);
@@ -108,7 +108,7 @@ for loc = locations
             before_MVDR_1noise = snr(rx_xA_jamsig, noise);
             disp("Before MVDR SNR: " + num2str(before_MVDR_1noise) + " dB");
 
-            [doas, averageMatrix] = estimateMUSIC(ura, rx_xA_jamsig_noise, noise, carrierFreq, averageMatrix, i, azimuth_range, elevation_range);
+            [doas, averageMatrix] = estimateMUSIC(uraNewW, rx_xA_jamsig_noise, noise, carrierFreq, averageMatrix, i, azimuth_range, elevation_range);
 
             fprintf("Expected DoA: \t%.2f \t%.2f \n", pathBtoA(1,1), pathBtoA(2,1))
 
@@ -123,13 +123,14 @@ for loc = locations
 
             % Perform MVDR Beamforming
             disp('Running MVDR Script...');
-            [signal, weights] = beamformerMVDR(ura, rx_xA_jamsig_noise, noise, doas, t, carrierFreq, propagation_path, show_plots);
+            [signal, weights] = beamformerMVDR(uraNewW, rx_xA_jamsig_noise, noise, doas, t, carrierFreq, propagation_path, show_plots);
             after_MVDR_1noise = snr(signal, noise(:,1));
             disp("After MVDR SNR: " + num2str(after_MVDR_1noise) + " dB");
 
             sweep(iteration,1) = loc;
             sweep(iteration,2) = bjammerPwr;
             sweep(iteration,3) = total_percent_error;
+            % sweep(iteration,3) = after_MVDR_1noise;
 
             if bjammerPwr == 0.01
                 colors(iteration,:) = [0.039 0.58 0.039];
@@ -145,9 +146,21 @@ for loc = locations
                 colors(iteration,:) = [0.6 0.051 0];
             end
             iteration = iteration + 1;
+            figure;
+            pattern(uraNewW,carrierFreq,'CoordinateSystem','polar','Type','powerdb'); 
+            view(50,20);
+            ax = gca;
+            ax.Position = [-0.15 0.1 0.9 0.8];
+            camva(4.5); 
+            campos([520 -250 200]);
         end
     end
 end
+
+% load desiredSynthesizedAntenna;
+
+% clf;
+
 
 % figure;
 % scatter(sweep(:,3), sweep(:,1), [], colors, 'filled')
